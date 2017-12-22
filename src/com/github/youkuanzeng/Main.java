f6eabe69acd0ed5542777553446b89bf89a85bac
YouKuanZeng <365085442@qq.com>
2017-12-22 23:18:17 +0800

    一些使用方法

--- src/com/github/youkuanzeng/Main.java
+++ src/com/github/youkuanzeng/Main.java
@@ -1,8 +1,27 @@
 package com.github.youkuanzeng;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<String> list = new ArrayList<>();
+        list.add("asdasd");
+        list.add("asdasd");
+        list.add("asdasd");
+        list.add("asdasd");
+        list.add("asdasd");
+        list.add("asdasd");
+        list.add("asdasd");
+        System.out.println(list);
+        // 在输入一个类名字的时候，如果它没有任何提示，并且它是一个红色的状态
+        // 那么，就输入：Alt + 回车
+        Set set = new HashSet();
+
+        // 省略文档和方法具体实现 ---> Ctrl + Shift + -
     }
 }
